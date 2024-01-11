f03a983acaeb20994de529efd30a066e93ca924c
nihal gaougaou <gaougaounihal@gmail.com>
2024-01-11 17:11:49 +0100

    initialisation du projet gestion produis avec class Produit

---
+++ GestionProduit/src/main/java/ma/assem_nihal/Produit.java
@@ -0,0 +1,56 @@
+package ma.assem_nihal;
+
+public class Produit {
+	private long id;
+	private String nom;
+	private double prix;
+	private int quantite;
+	
+	public Produit(long id,String nom,double prix,int quantite) 
+	{
+		this.id=id;
+		this.nom=nom;
+		this.prix=prix;
+		this.quantite=quantite;
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Produit [id=" + id + ", nom=" + nom + ", prix=" + prix + ", quantite=" + quantite + "]";
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	
+}
+
